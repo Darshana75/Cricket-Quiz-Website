6a800d201cdac3b1bf911627ef467cf2c3f528c8
Darshana Prabath <84192420+Darshana75@users.noreply.github.com>
2023-09-02 23:21:30 +0530

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,112 @@
+let questions = [
+    {
+        numb: 1,
+        question: "Which cricketer has scored the most runs at the World Cup?",
+        answer: "A. Sachin Tendulkar",
+        options: [
+            "A. Sachin Tendulkar",
+            "B. Kumar Sangakkara",
+            "C. Sanath Jayasuriya",
+            "D. Ricky Ponting"
+        ]
+    },
+    {
+        numb: 2,
+        question: "Who was the first Sri Lankan player to play 100 Tests?",
+        answer: "C. S Jayasuriya",
+        options: [
+            "A. M Jayawardene",
+            "B. M Muralitharan",
+            "C. S Jayasuriya",
+            "D. A de Silva"
+        ]
+    },
+    {
+        numb: 3,
+        question: "In which year were the World Cup matches reduced to 50 overs from the previous 60 overs?",
+        answer: "D. 1987",
+        options: [
+            "A. 1983",
+            "B. 1979",
+            "C. 1992",
+            "D. 1987"    
+        ]
+    },
+    {
+        numb: 4,
+        question: "Who was the first Sri Lankan player to have scored a century and taken 3 wickets in the same ODI match?",
+        answer: "D. Aravinda de Silva",
+        options: [
+            "A. Angelo Mathews",
+            "B. Sanath Jayasuriya",
+            "C. TM Dilshan",
+            "D. Aravinda de Silva"
+        ]
+    },
+    {
+        numb: 5,
+        question: "Who won the first ever Cricket World Cup in 1975?",
+        answer: "B. West Indies",
+        options: [
+            "A. Australia",
+            "B. West Indies",
+            "C. India",
+            "D. England"
+        ]
+    },
+    {
+        numb: 6,
+        question: "When was the Ashes first played?",
+        answer: "C. 1877",
+        options: [
+            "A. 1872",
+            "B. 1882",
+            "C. 1877",
+            "D. 1887"
+        ]
+    },
+    {
+        numb: 7,
+        question: "What is the largest cricket stadium in the world?",
+        answer: "D. Narendra Modi Stadium",
+        options: [
+            "A. Eden Gardens",
+            "B. Melbourne Cricket Club",
+            "C. Lords",
+            "D. Narendra Modi Stadium"
+        ]
+    },
+    {
+        numb: 8,
+        question: "How many ways are there of getting out?",
+        answer: "C. 10",
+        options: [
+            "A. 7",
+            "B. 8",
+            "C. 10",
+            "D. 9"
+        ]
+    },
+    {
+        numb: 9,
+        question: "How long is the wicket on a cricket pitch?",
+        answer: "A. 22 yards",
+        options: [
+            "A. 22 yards",
+            "B. 24 yards",
+            "C. 18 yards",
+            "D. 20 yards"
+        ]
+    },
+    {
+        numb: 10,
+        question: "Who did England beat in the final of the 2019 Cricket World Cup?",
+        answer: "D. New Zealand",
+        options: [
+            "A. Australia",
+            "B. West Indies",
+            "C. India",
+            "D. New Zealand"
+        ]
+    },
+];
